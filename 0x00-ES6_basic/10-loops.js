590bfc6b022415b49b8a5276c17fc082c982a916
Abdullahi Abdulrofiu <abdulrofiuabdullahi@gmail.com>
2024-07-27 02:27:56 +0100

    #10-debg

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,6 +1,6 @@
 export default function appendToEachArrayValue(array, appendString) {
   const newArray = array;
-  for (const idx in array) {
+  for (const idx of array) {
     const value = array.indexOf(idx);
     newArray[value] = appendString + idx;
   }
