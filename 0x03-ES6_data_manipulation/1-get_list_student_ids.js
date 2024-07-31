069cdee44b54f3c8f01631bfb9e4bcc66118374f
Abdullahi Abdulrofiu <abdulrofiuabdullahi@gmail.com>
2024-07-31 01:03:43 +0100

    #1

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,4 +1,3 @@
-// import getListStudents from './0-get_list_students'
 // returns an array of ids from a list of object
 export default function getListStudentIds(studentList) {
   if (!Array.isArray(studentList)) return [];
